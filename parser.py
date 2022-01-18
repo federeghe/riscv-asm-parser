ae58452c863c26be51da2bb0a4f426243ea60d6c
Federico Reghenzani <federico.reghenzani@polimi.it>
2022-01-18 16:14:01 +0100

    Code cleanup

--- parser.py
+++ parser.py
@@ -1,23 +1,45 @@
 #!/bin/python3
 import pyparsing as pp
+import sys
 
-pp.ParserElement.setDefaultWhitespaceChars(' \t') 
+###################################################################################################
+# Parser token definitions
+###################################################################################################
 
-# Numbers
-token_number = pp.Combine(pp.Optional(pp.Char('-')) + (pp.Word(pp.nums) ^ (pp.Literal("0x") + pp.Word(pp.hexnums))))
+# First of all, the parser should not ignore line ends
+pp.ParserElement.setDefaultWhitespaceChars(' \t')
 
-# Functions
-token_identifier = pp.pyparsing_common.identifier 
-token_argument = pp.Combine((token_identifier ^ pp.Char('*'))[...], adjacent=False, joinString=" ")
+## Numbers
+# They can have a +, a -, or be hex
+token_number_hex = pp.Literal("0x") + pp.Word(pp.hexnums)
+token_number = pp.Combine(pp.Optional(pp.Char('-') ^ pp.Char('+')) + (pp.Word(pp.nums) ^ token_number_hex))
+
+## Functions
+token_identifier = pp.pyparsing_common.identifier   # This is a standard identifier for Python, should be ok also for C
+# Arguments can be a sequence of types (like unsigned int **) with an arbitrary number of *
+token_argument = pp.Combine((token_identifier)[...] + pp.Char('*')[...], adjacent=False, joinString=" ")
 token_arguments = pp.Optional(token_argument) ^ (token_argument + (pp.Suppress(pp.Char(',')) + token_argument)[...])
 token_function_full = pp.Group(token_identifier + pp.Suppress("(") + token_arguments + pp.Suppress(")"))
-token_functions = token_function_full + pp.Suppress(pp.Char(':'))
+token_functions = token_function_full + pp.Suppress(pp.Char(':'))   # Function line declaration
 
-# Labels
+## Labels
+# A label can be in one of the following formats:
+# global_label:
+# .local_label:
+# global_label.something:
+# global_label.1234:
 token_label_name = pp.Combine(pp.Optional(".") + pp.Optional(pp.Word(pp.nums)) + token_identifier + pp.Optional("." + pp.Optional(pp.Word(pp.nums)) + pp.Optional(token_identifier)))("name")
 token_labels = token_label_name + pp.Suppress(":")
 
-# Instructions
+## Instructions
+# For instructions the main difficulty is matching all the parameters type:
+# - Numbers
+# - Labels
+# - Relocation like %h1(abc)
+# - A numeric register
+# - A named register
+# - Any offset of the previous (including %h1(abc)(a3))
+# Instruction names can also contain dots
 token_instruction_parameter_num = token_number
 token_instruction_parameter_label = token_label_name
 token_instruction_parameter_relocation = pp.Group(pp.Combine(pp.Char('%')+ pp.pyparsing_common.identifier) + pp.Suppress("(") + (pp.pyparsing_common.identifier ^ token_instruction_parameter_label) + pp.Suppress(")"))
@@ -30,19 +52,32 @@ token_instruction_parameter = token_instruction_parameter_all ^ token_instructio
 token_instruction_name = pp.Combine(pp.Word(pp.alphanums) + (pp.Char('.') + pp.Word(pp.alphanums))[...])
 token_instructions = token_instruction_name("name") + pp.Optional(token_instruction_parameter("param") + (pp.Suppress(",")+token_instruction_parameter("param"))[...])
 
-# Pseudo OPS
-token_pseudoops_arg_sym = pp.Combine(token_identifier + pp.Optional(pp.Char(',') + token_number + pp.Char(',') + token_number) + pp.Optional("," + token_identifier), adjacent=False, joinString=" ")
+## Pseudo OPS
+# There are several pseudo ops with different syntax. Probably some are missing
+token_pseudoops_arg_sym = pp.Combine(pp.Optional('.') + token_identifier + pp.Optional(pp.Char(',') + token_number + pp.Char(',') + token_number) + pp.Optional("," + token_identifier), adjacent=False, joinString=" ")
 token_pseudoops_arg_str = pp.QuotedString('"')
 token_pseudoops_arg_lbl = pp.Char('.')+ pp.pyparsing_common.identifier
 token_pseudoops_arg = token_number ^ token_pseudoops_arg_sym ^ token_pseudoops_arg_str ^ token_pseudoops_arg_lbl
 token_pseudoops = pp.Suppress('.') + (pp.Word(pp.nums) ^ pp.pyparsing_common.identifier) + token_pseudoops_arg
 
+## Line
+# Define the new line constant
 NL = pp.Suppress(pp.LineEnd())
 
+# General line format
 token_all = pp.Group(token_functions("function") ^ token_labels("label") ^ token_instructions("instruction") ^ token_pseudoops("pseudop")) + NL
-lines_all = token_all[1,...] 
+lines_all = token_all[1,...]   # Whole file format
 
-lines_all.ignore("#" + pp.restOfLine) 
-parsed = lines_all.parseFile("examples/sha1_64.s", parseAll=True)
+###################################################################################################
+# Parser config and run definitions
+###################################################################################################
+
+lines_all.ignore("#" + pp.restOfLine)   # Ignore comments starting with #
+
+if len(sys.argv) < 2:
+    print("Usage: python parser.py file_name.s")
+    sys.exit()
+
+parsed = lines_all.parseFile(sys.argv[1], parseAll=True)
 print(parsed.dump())
 
